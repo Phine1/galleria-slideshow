9858787126daabee8570e249ab3aa4097f7367bd
ntiBoateng <gideon.boateng@amalitech.org>
2022-02-08 11:12:00 -0800

    text on images added feature added

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ const div = document.getElementById("div")
     .then((response) => response.json())
     .then((data)=> {
         data.forEach((element) => {
-          div.innerHTML += `
-          <section class= grid-item">
-            
-              <div>
-                <h2>${element.name}</h2>
-                <h3>${element.year}</h3>
-                <p>${element.artist.name}</p>
+          div.innerHTML += ` 
+          <section class= "image-wrapper grid-item">
+          <a class="grid-item__link" href="">
+              <div class="grid-item__text">
+                <h2 class="heading heading--2 white">${element.name}</h2>
+                
+                <p class="subhead subhead--2 white--opacity">${element.artist.name}</p>
               </div>
-              <img src="${element.images.thumbnail}" alt="${element.name}, author ${element.artist.name}">
+              <img class="grid-item__image" src="${element.images.thumbnail}" alt="${element.name}, author ${element.artist.name}">
             </a>
           </section>`
       
 //a feature from masonry grid for vertical alignment
 const macy = Macy({
   container: '.div',
-  columns: 4,
+  columns:4,
   margin: 24,
   trueOrder: false,
   breakAt: {
