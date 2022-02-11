9f264e14804471164786f751adc639fb30958ba5
Solomon Owusu Sarfo <solomonsarfo045@gmail.com>
2022-02-11 12:16:34 +0000

    apply hover on homepage

--- public/script.js
+++ public/script.js
@@ -20,79 +20,120 @@ fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
     one.innerHTML = ` 
-                <h2 class="heading">${data[0].name}</h2>
-                <p class="subhead">${data[0].artist.name}</p>
-              <img class="grid" src="${data[0].images.thumbnail}" alt="${data[0].name}, author ${data[0].artist.name}">
-              </div>
+        <div class="image_text">
+            <h2 class="heading">${data[0].name}</h2>
+            <p class="subhead">${data[0].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[0].images.thumbnail}" alt="${data[0].name}, author ${data[0].artist.name}">
+            
           `;
-    two.innerHTML = `  <div class='render'>
-     <h2 class="heading">${data[4].name}</h2> 
-          <p class="subhead">${data[4].artist.name}</p>
+    two.innerHTML = ` 
+        <div class="image_text">
+            <h2 class="heading">${data[4].name}</h2> 
+            <p class="subhead">${data[4].artist.name}</p>
+        </div>  
         <img class="grid" src="${data[4].images.thumbnail}" alt="${data[4].name}, author ${data[4].artist.name}">
+          
  `;
-    three.innerHTML = ` 
-    <h2 class="heading">${data[8].name}</h2>
-    <p class="subhead">${data[8].artist.name}</p>
-    <img class="grid" src="${data[8].images.thumbnail}" alt="${data[8].name}, author ${data[8].artist.name}">
+    three.innerHTML = `
+        <div class="image_text">
+            <h2 class="heading">${data[8].name}</h2>
+            <p class="subhead">${data[8].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[8].images.thumbnail}" alt="${data[8].name}, author ${data[8].artist.name}">
+        
 `;
     four.innerHTML = `
-<h2 class="heading">${data[11].name}</h2>
-<p class="subhead">${data[11].artist.name}</p>
-<img class="grid" src="${data[11].images.thumbnail}" alt="${data[11].name}, author ${data[11].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[11].name}</h2>
+            <p class="subhead">${data[11].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[11].images.thumbnail}" alt="${data[11].name}, author ${data[11].artist.name}">
+    
  `;
     five.innerHTML = `
-<h2 class="heading">${data[1].name}</h2>
-<p class="subhead">${data[1].artist.name}</p>
-<img class="grid" src="${data[1].images.thumbnail}" alt="${data[1].name}, author ${data[1].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[1].name}</h2>
+            <p class="subhead">${data[1].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[1].images.thumbnail}" alt="${data[1].name}, author ${data[1].artist.name}">
+    
 `;
     six.innerHTML = ` 
-<h2 class="heading">${data[5].name}</h2>
-<p class="subhead">${data[5].artist.name}</p>
-<img class="grid" src="${data[5].images.thumbnail}" alt="${data[5].name}, author ${data[5].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[5].name}</h2>
+            <p class="subhead">${data[5].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[5].images.thumbnail}" alt="${data[5].name}, author ${data[5].artist.name}">
   `;
     seven.innerHTML = `
-<h2 class="heading">${data[9].name}</h2>
-<p class="subhead">${data[9].artist.name}</p>
-<img class="grid" src="${data[9].images.thumbnail}" alt="${data[9].name}, author ${data[9].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[9].name}</h2>
+            <p class="subhead">${data[9].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[9].images.thumbnail}" alt="${data[9].name}, author ${data[9].artist.name}">
  `;
     eight.innerHTML = `
-<h2 class="heading">${data[12].name}</h2>
-<p class="subhead">${data[12].artist.name}</p>
-<img class="grid" src="${data[12].images.thumbnail}" alt="${data[12].name}, author ${data[12].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[12].name}</h2>
+            <p class="subhead">${data[12].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[12].images.thumbnail}" alt="${data[12].name}, author ${data[12].artist.name}">
+      
   `;
     nine.innerHTML = `
-<h2 class="heading">${data[2].name}</h2>
-<p class="subhead">${data[2].artist.name}</p>
-<img class="grid" src="${data[2].images.thumbnail}" alt="${data[2].name}, author ${data[2].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[2].name}</h2>
+            <p class="subhead">${data[2].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[2].images.thumbnail}" alt="${data[2].name}, author ${data[2].artist.name}">
+    
  `;
     ten.innerHTML = `
-<h2 class="heading">${data[6].name}</h2>
-<p class="subhead">${data[6].artist.name}</p>
-<img class="grid" src="${data[6].images.thumbnail}" alt="${data[6].name}, author ${data[6].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[6].name}</h2>
+            <p class="subhead">${data[6].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[6].images.thumbnail}" alt="${data[6].name}, author ${data[6].artist.name}">
+    
 `;
     eleven.innerHTML = `
-<h2 class="heading">${data[13].name}</h2>
-<p class="subhead">${data[13].artist.name}</p>
-<img class="grid" src="${data[13].images.thumbnail}" alt="${data[13].name}, author ${data[13].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[13].name}</h2>
+            <p class="subhead">${data[13].artist.name}</p>
+        </div>
+            <img class="grid" src="${data[13].images.thumbnail}" alt="${data[13].name}, author ${data[13].artist.name}">
+   
  `;
     twelve.innerHTML = ` 
-<h2 class="heading">${data[3].name}</h2>
-<p class="subhead">${data[3].artist.name}</p>
-<img class="grid" src="${data[3].images.thumbnail}" alt="${data[3].name}, author ${data[3].artist.name}">
+        <div class="image_text">
+            <h2 class="heading">${data[3].name}</h2>
+            <p class="subhead">${data[3].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[3].images.thumbnail}" alt="${data[3].name}, author ${data[3].artist.name}">
 `;
-    thirteen.innerHTML = ` 
-<h2 class="heading">${data[7].name}</h2>
-<p class="subhead">${data[7].artist.name}</p>
-<img class="grid" src="${data[7].images.thumbnail}" alt="${data[7].name}, author ${data[7].artist.name}">
+    thirteen.innerHTML = `
+        <div class="image_text">
+            <h2 class="heading">${data[7].name}</h2>
+            <p class="subhead">${data[7].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[7].images.thumbnail}" alt="${data[7].name}, author ${data[7].artist.name}">
+    
 `;
-    fourteen.innerHTML = ` 
-<h2 class="heading">${data[10].name}</h2>
-<p class="subhead">${data[10].artist.name}</p>
-<img class="grid" src="${data[10].images.thumbnail}" alt="${data[10].name}, author ${data[10].artist.name}">
+    fourteen.innerHTML = `
+        <div class="image_text">
+            <h2 class="heading">${data[10].name}</h2>
+            <p class="subhead">${data[10].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[10].images.thumbnail}" alt="${data[10].name}, author ${data[10].artist.name}">
+    
 `;
-    fifteen.innerHTML = ` 
-<h2 class="heading">${data[14].name}</h2>
-<p class="subhead">${data[14].artist.name}</p>
-<img class="grid" src="${data[14].images.thumbnail}" alt="${data[14].name}, author ${data[14].artist.name}">
+    fifteen.innerHTML = `
+        <div class="image_text">
+            <h2 class="heading">${data[14].name}</h2>
+            <p class="subhead">${data[14].artist.name}</p>
+        </div>
+        <img class="grid" src="${data[14].images.thumbnail}" alt="${data[14].name}, author ${data[14].artist.name}">
+    
 `;
   });
